c05791c7c317a8f61cdf1722add1b6f1bbe67247
joe-software <joewebdev@outlook.com>
2024-10-19 00:44:58 +0100

    Removed .then from async controllers - using async/await only

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -15,45 +15,42 @@ export class CarsController {
 @Render('home')
 async root() {
 // route to service and await response (all entries in db) - then include as data sent to view
-let returnData
-await this.carService.findAll().then( (data) => {
-    returnData = data
-})
-return {carData: returnData}
+let data = await this.carService.findAll()
+return {carData: data}
 }
 
-// Get request - /cars
+// Get request - /cars/delete
 @Get('delete')
 // render delete.edge - within /src/views - views path added in main.ts
 @Render('delete')
 async test() {
 // route to service and await response (all entries in db) - then include as data sent to view
-let returnData
-await this.carService.findAll().then( (data) => {
-    returnData = data
-})
-return {carData: returnData}
+let data = await this.carService.findAll()
+return {carData: data}
 }
 
 
     // POST /cars/car-post
     @Post('car-post')
-    @Render('successful-post')
+    @Render('successful-edit')
     addACar(@Body() createCarDto: CreateCarDto){
         this.carService.create(createCarDto)
+        return {status: 'added'}
  }
         
 
     @Post('car-delete')
-    @Render('successful-post')
-    deleteACar(@Body() body: {}){
+    @Render('successful-edit')
+    deleteACar(@Body() body: {}): {}{
         this.carService.deleteOneCar(body)
+        return {status: 'deleted'}
     }
 
     @Post('car-update')
-    @Render('successful-post')
-    updateACar(@Body() body: {}){
+    @Render('successful-edit')
+    updateACar(@Body() body: {}): {}{
         this.carService.updateOneCar(body)
+        return {status: 'edited'}
     }
 }
 
