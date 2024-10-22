865880666e77c5201649758dcfe3e541a93b8470
joe-software <joewebdev@outlook.com>
2024-10-22 22:49:55 +0100

    Added landing page view for app at /

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,9 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('') 
-   getCarData(): {} {
-
-    return 'route for / does not return, try /cars'
+  @Render('home-app')
+   getCarData(): undefined{
   }
 
   }
