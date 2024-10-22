23534a573c3a5c02508f8e40cb2b3721703da278
joe-software <joewebdev@outlook.com>
2024-10-22 22:05:02 +0100

    created route and view for interface to delete users - /users/delete

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,9 +17,19 @@ export class UsersController {
 
     @Get('add')
     @Render('add-entry-user')
-    emptyFunction(){
-        return
+    emptyAddFunction(){
+        
+    }   
+
+    @Get('delete')
+    @Render('delete-user')
+    async findAllForDeleteView() {
+        // route to service and await response (all entries in db) - then include as data sent to view
+        let data = await this.userService.findAll()
+        return {userData: data}
     }
+   
+
 
     @Post('user-post')
     addAUser(@Body() createUserDto: CreateUserDto){
