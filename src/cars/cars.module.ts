e5ce9cab72cffdbe28e434445f0dbcbeb43b7e1a
joe-software <joewebdev@outlook.com>
2024-10-27 11:02:10 +0000

    migrated db and schema to Prism/Sqlite - working through Car service CRUD operations - delete still WIP. Then will begin to migrate User services

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { CarsController } from './cars.controller';
 import { CarsService } from './cars.service';
 import { ConfigModule } from '@nestjs/config';
+import {PrismaService} from '../prisma.service'
 
 
 const dotenv = require('dotenv');
 dotenv.config();
-const uri = process.env.DBSTRING
 
 @Module({
   //import mongoose/scheme data into the module and relevant controllers and providers
   imports: [ConfigModule.forRoot()],
   controllers: [CarsController],
-  providers: [CarsService]
+  providers: [CarsService, PrismaService]
 })
 export class CarsModule {}
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { User, Car, Prisma } from '@prisma/client';
 
 // create interface to be used when creating car data objects from request data
 interface CarDataInterface {
@@ -12,40 +14,31 @@ interface CarDataInterface {
 // highlight injectable so may be injected into other components
 @Injectable()
 export class CarsService {
-        // include mongoose/schema information within class
-        // constructor(@InjectModel(CarDataCollection.name) private carModel: Model<CarDataCollection>){}
-        constructor(){}
+        
+        constructor(private prisma: PrismaService){}
 
             // service which requests and returns all car data from db - async so doesnt stop other processes and can wait for response and promise representsa the eventual completion
-            async findAll() {
-            return 'string'
+            async findAll(): Promise<Car []> {
+                return this.prisma.car.findMany({
+                });
         
           }
             // service which takes input data from @body and creates an object which is then saved as new data in db 
-            async create(reqCarData) {
-                let inputCarData: CarDataInterface = {
-                    'brand': reqCarData['car-brand-input'],
-                    'model': reqCarData['car-model-input'],
-                    'date': reqCarData['car-year-input'],
-                    'colour': reqCarData['car-colour-input']
-                }
+            async create(data: Prisma.CarCreateInput) {
+                return this.prisma.car.create({data
+                });
              
           }
 
-
           // service which takes input data from @body and deletes a data entry from db which matches the @body mongoid value 
-          async deleteOneCar(requestId){
-                let deleteId: string = requestId['mongoid']
+          async deleteOneCar(requestId): Promise <Car>{
+                let dataId: number = Number(requestId['id'])
+                return this.prisma.car.delete({where: {id:dataId}})
           }
 
           // service which creates a new data object from @body - then matches @body mongoid with db, and replaces existing db data for entry with new data object
           async updateOneCar(reqCarData) {
-            let putId: string = reqCarData['mongoid']
-            let updateCarData: CarDataInterface = {
-                'brand': reqCarData['car-brand-update'],
-                'model': reqCarData['car-model-update'],
-                'date': reqCarData['car-date-update'],
-                'colour': reqCarData['car-colour-update']
-            }
+            let dataId: number = Number(reqCarData['id'])
+            return this.prisma.car.update({data: reqCarData, where: {id:dataId}})
       }
 }
